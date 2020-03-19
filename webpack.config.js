8f51ce8042b0c6ebf907cdb9a9ac1cdd9c87ecb9
MrZhiin <mrzhiin@gmail.com>
2020-03-19 23:02:21 +0800

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const fse = require("fs-extra");
 
 const OUTPUT_PATH = path.resolve(__dirname, "dist");
 
-module.exports = {
+const common = {
   mode: "production",
   entry: {
     main: "./src/index.ts"
@@ -19,8 +23,11 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
-        exclude: /node_modules/
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true
+        }
       },
       {
         test: /\.css$/i,
@@ -49,7 +56,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [new Dotenv()],
+  plugins: [new Dotenv(), new webpack.ProgressPlugin()],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     alias: {
@@ -58,5 +65,47 @@ module.exports = {
       "react-dom/test-utils": "preact/test-utils",
       "react-dom": "preact/compat"
     }
+  }
+};
+
+const development = {
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8000,
+    host: "0.0.0.0",
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/dev.html",
+      inject: "head"
+    })
+  ]
+};
+
+const standard = {
+  output: {
+    filename: `modis.min.js`
+  }
+};
+
+const slim = {
+  output: {
+    filename: `modis.slim.min.js`
+  },
+  externals: {
+    "leancloud-storage": "AV"
+  }
+};
+
+module.exports = (env = {}) => {
+  if (env.production) {
+    fse.removeSync(common.output.path);
+    return [merge(common, standard), merge(common, slim)];
+  } else {
+    return merge(common, development);
   }
 };
